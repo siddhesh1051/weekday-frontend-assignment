7509b1bc848fa6eaa4350df18edc85ba4328a720
siddhesh1051 <siddhesh1052001@gmail.com>
2024-05-11 15:57:31 +0530

    FIX: Added contants for better redablity and maintenance

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
   FormControl,
 } from "@mui/material";
 import AboutCompany from "./components/AboutCompany";
+import { COMPANY_NAMES, LOCATIONS, ROLES, TECH_STACK } from "./constants/page";
 
 function App() {
   const [getJobsData, getJobsDataResult] = useGetSampleJdMutation();
@@ -145,13 +146,7 @@ function App() {
             )}
           >
             {/* <MenuItem value="Select Location(s)">Select Location(s)</MenuItem> */}
-            {[
-              { label: "Delhi NCR", val: "delhi ncr" },
-              { label: "Mumbai", val: "mumbai" },
-              { label: "Remote", val: "remote" },
-              { label: "Chennai", val: "chennai" },
-              { label: "Bangalore", val: "bangalore" },
-            ].map((option) => (
+            {LOCATIONS.map((option) => (
               <MenuItem key={option.val} value={option.val}>
                 {option.label}
               </MenuItem>
@@ -168,13 +163,7 @@ function App() {
             label="Select Role(s)"
             inputProps={{ "aria-label": "Select Roles" }}
           >
-            {[
-              { label: "Frontend", val: "frontend" },
-              { label: "iOS", val: "ios" },
-              { label: "Android", val: "android" },
-              { label: "Tech Lead", val: "tech lead" },
-              { label: "Backend", val: "backend" },
-            ].map((option) => (
+            {ROLES.map((option) => (
               <MenuItem key={option.val} value={option.val}>
                 {option.label}
               </MenuItem>
@@ -192,208 +181,7 @@ function App() {
             label="Select Companies"
             inputProps={{ "aria-label": "Select Companies" }}
           >
-            {[
-              {
-                label: "Dropbox",
-                val: "Dropbox",
-              },
-              {
-                label: "LG",
-                val: "LG",
-              },
-              {
-                label: "Sony",
-                val: "Sony",
-              },
-              {
-                label: "Adobe Systems",
-                val: "Adobe Systems",
-              },
-              {
-                label: "HP",
-                val: "HP",
-              },
-              {
-                label: "eBay",
-                val: "eBay",
-              },
-              {
-                label: "Tencent",
-                val: "Tencent",
-              },
-              {
-                label: "Apple",
-                val: "Apple",
-              },
-              {
-                label: "Asus",
-                val: "Asus",
-              },
-              {
-                label: "Intel Corporation",
-                val: "Intel Corporation",
-              },
-              {
-                label: "Rakuten",
-                val: "Rakuten",
-              },
-              {
-                label: "Samsung",
-                val: "Samsung",
-              },
-              {
-                label: "Dell Technologies",
-                val: "Dell Technologies",
-              },
-              {
-                label: "Cisco",
-                val: "Cisco",
-              },
-              {
-                label: "Oracle",
-                val: "Oracle",
-              },
-              {
-                label: "Baidu",
-                val: "Baidu",
-              },
-              {
-                label: "Amazon",
-                val: "Amazon",
-              },
-              {
-                label: "Olympus",
-                val: "Olympus",
-              },
-              {
-                label: "Alibaba",
-                val: "Alibaba",
-              },
-              {
-                label: "GoPro",
-                val: "GoPro",
-              },
-              {
-                label: "Twitter",
-                val: "Twitter",
-              },
-              {
-                label: "ZTE",
-                val: "ZTE",
-              },
-              {
-                label: "Netflix",
-                val: "Netflix",
-              },
-              {
-                label: "MasterCard",
-                val: "MasterCard",
-              },
-              {
-                label: "Facebook",
-                val: "Facebook",
-              },
-              {
-                label: "IBM",
-                val: "IBM",
-              },
-              {
-                label: "Intel",
-                val: "Intel",
-              },
-              {
-                label: "Google",
-                val: "Google",
-              },
-              {
-                label: "Huawei",
-                val: "Huawei",
-              },
-              {
-                label: "Adobe",
-                val: "Adobe",
-              },
-              {
-                label: "Pandora",
-                val: "Pandora",
-              },
-              {
-                label: "Nikon",
-                val: "Nikon",
-              },
-              {
-                label: "Lyft",
-                val: "Lyft",
-              },
-              {
-                label: "Spotify",
-                val: "Spotify",
-              },
-              {
-                label: "PayPal",
-                val: "PayPal",
-              },
-              {
-                label: "Visa",
-                val: "Visa",
-              },
-              {
-                label: "Adobe Inc.",
-                val: "Adobe Inc.",
-              },
-              {
-                label: "Sharp",
-                val: "Sharp",
-              },
-              {
-                label: "Qualcomm",
-                val: "Qualcomm",
-              },
-              {
-                label: "Yahoo",
-                val: "Yahoo",
-              },
-              {
-                label: "Panasonic",
-                val: "Panasonic",
-              },
-              {
-                label: "Xiaomi",
-                val: "Xiaomi",
-              },
-              {
-                label: "Microsoft",
-                val: "Microsoft",
-              },
-              {
-                label: "Tesla",
-                val: "Tesla",
-              },
-              {
-                label: "Epson",
-                val: "Epson",
-              },
-              {
-                label: "Airbnb",
-                val: "Airbnb",
-              },
-              {
-                label: "Canon",
-                val: "Canon",
-              },
-              {
-                label: "Vimeo",
-                val: "Vimeo",
-              },
-              {
-                label: "Uber",
-                val: "Uber",
-              },
-              {
-                label: "LinkedIn",
-                val: "LinkedIn",
-              },
-            ].map((option) => (
+            {COMPANY_NAMES.map((option) => (
               <MenuItem key={option.val} value={option.val}>
                 {option.label}
               </MenuItem>
@@ -411,11 +199,7 @@ function App() {
             label="Select Tech Stacks"
             inputProps={{ "aria-label": "Select Tech Stacks" }}
           >
-            {[
-              { label: "React", option: "react" },
-              { label: "Javascript", option: "javascript" },
-              { label: "Java", option: "java" },
-            ].map((option) => (
+            {TECH_STACK.map((option) => (
               <MenuItem key={option.option} value={option.option}>
                 {option.label}
               </MenuItem>
